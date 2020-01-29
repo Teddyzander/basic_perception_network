ec080892e627a5f93f4399d58ee1293cc50c6485
Teddyzander <edwardalexandersmall@hotmail.co.uk>
2020-01-29 18:43:28 +0000

    Created class that should train a simple neuron
    Removed unneccessary stuff at the start of the file

--- perceptron.py
+++ perceptron.py
@@ -1,52 +1,6 @@
 import numpy as np
 
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-
-def sigmoid_derivative(x):
-    return x * (1 - x)
-
-
-# define training data
-# rule: return 1 if first input is 1
-training_inputs = np.array([[0, 0, 1],
-                            [1, 1, 1],
-                            [1, 0, 1],
-                            [0, 1, 1]])
-
-training_outputs = np.array([[0, 1, 1, 0]]).T
-
-# assign weights
-
-np.random.seed(1)
-
-weights = 2 * np.random.random((3, 1)) - 1
-
-print("Weights before training: ", weights)
-
-for i in range(20000):
-
-    if i % 100000 == 0:
-        print(i)
-
-    input_layer = training_inputs
-
-    # calculate an output
-    outputs = sigmoid(np.dot(input_layer, weights))
-
-    # find how wrong the calculated output was against the expected output
-    error = training_outputs - outputs
-
-    # calculate adjustment
-    adjustment = error * sigmoid_derivative(outputs)
-
-    weights += np.dot(input_layer.T, adjustment)
-
-print("Weights after training: ", weights)
-print("outputs after training: ", outputs)
-
 class PerceptronNetwork:
 
     def __init__(self):
@@ -55,11 +9,28 @@ class PerceptronNetwork:
         # create weights psuedo-randomly between -1 and 1 with a mean of 0
         self.synaptic_weights = 2 * np.random.random((3, 1)) - 1
 
+    # function to calculate the value of the sigmoid function at a given point x
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
 
+    # function to calculate the slope of the sigmoid function at a given point x
     def sigmoid_derivative(self, x):
         return x * (1 - x)
 
+    # function to train the neural network
+    def train(self, training_inputs, training_outputs, training_it):
+        for it in range(training_it):
+            output = self.think(training_inputs)
+            error = training_outputs - output
+            adjustments = np.dot(training_inputs.T, error * self.sigmoid_derivative(output))
 
+            # make the adjustments (back propagation)
+            self.synaptic_weights += adjustments
 
+    def think(self, inputs):
+        # need to make sure inputs are floats
+        inputs = inputs.astype(float)
+
+        output = self.sigmoid(np.dot(inputs, self.synaptic_weights))
+
+        return output
